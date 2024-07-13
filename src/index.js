f4076885ea3f8d558865b5c1b7d5831d1f59d895
amtffofa99 <254421677@qq.com>
2024-07-13 15:14:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.nas2023.cn": dockerHub,
+  "quay.nas2023.cn": "https://quay.io",
+  "gcr.nas2023.cn": "https://gcr.io",
+  "k8s-gcr.nas2023.cn": "https://k8s.gcr.io",
+  "k8s.nas2023.cn": "https://registry.k8s.io",
+  "ghcr.nas2023.cn": "https://ghcr.io",
+  "cloudsmith.nas2023.cn": "https://docker.cloudsmith.io",
+  "ecr.nas2023.cn": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
